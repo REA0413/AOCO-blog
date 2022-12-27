c1eb04ae6a4b0d4124220344d126f1d6c5d660bc
Rea <andreasyanuarhanaekan@gmail.com>
2022-12-27 15:25:24 +0700

    Add gunicorn line based on Q&A

--- forms.py
+++ forms.py
@@ -2,7 +2,6 @@ from flask_ckeditor import CKEditorField
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, EmailField, validators
 from wtforms.validators import DataRequired, URL, Email
-import email_validator
 
 
 ##WTForm
